b50ea94d7df2f1931055af4505675db91ef7cd0a
Carson Buttars <carson@congo>
2024-07-18 16:25:03 -0600

    Add debug wrapper and implement in simple cases.

--- web-scripts/lib/debug.py
+++ web-scripts/lib/debug.py
@@ -1,7 +1,7 @@
 def debug(title, message):
     print('[ ' + title + ' ]: ' + message)
 
-def debug_error(soup, location, field, info='', return_type):
+def debug_error(soup, location, field, return_type, info = ''):
     if not isinstance(return_type, type):
         raise TypeError
 
@@ -19,4 +19,14 @@ def debug_error(soup, location, field, info='', return_type):
         output = ''
 
     return output
+
+def error_wrap(location = '', field = '', return_type = '', info = ''):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            try:
+                return function
+            except:
+                return debug_error(self.soup, location, field, return_type, info)
+        return wrapper
+    return decorator
 
--- web-scripts/lib/other_info.py
+++ web-scripts/lib/other_info.py
@@ -1,12 +1,16 @@
+import debug
+
 class referee_info:
 
     def __init__(self, soup):
         self.soup = soup
 
+    @debug.error_wrap('referee_info', 'name', str)
     def name(self):
         self.Name = self.soup.select('h1 span')[0].text
         return self.Name
 
+    @debug.error_wrap('referee_info', 'number', int)
     def number(self):
         try:
             self.Number = int(self.soup.find('text').text)
@@ -14,17 +18,22 @@ class referee_info:
             self.Number = 0
         return self.Number
 
+    @debug.error_wrap('referee_info', 'birthday', str)
     def birthday(self):
         try:
-            self.Birthday = [x.text for x in self.soup.select('#meta p') if 'Born:' in x.text][0].replace('Born: ', '').split(' in ')[0].replace('Born:\n', '').replace('\n', '').replace('   ', '')
+            birthday_raw = [x.text for x in self.soup.select('#meta p') if 'Born:' in x.text][0]
         except:
             self.Birthday = ''
+        else:
+            birthday_raw = birthday_raw.replace('Born: ', '').split(' in ')[0]
+            self.Birthday = birthday_raw.replace('Born:\n', '').replace('\n', '').replace('   ', '')
         return self.Birthday
 
     def generate_referee_id(self, prev):
         self.Referee_ID = prev + 1
         return self.Referee_ID
 
+    @debug.error_wrap('referee_info', 'referee_id', int)
     def referee_id(self):
         return self.Referee_ID
     
@@ -41,17 +50,23 @@ class executive_info:
     def __init__(self, soup):
         self.soup = soup
 
+    @debug.error_wrap('executive_info', 'name', str)
     def name(self):
         self.Name = self.soup.select('h1 span')[0].text
         return self.Name
 
+    @debug.error_wrap('executive_info', 'birthday', str)
     def birthday(self):
         try:
-            self.Birthday = [x.text for x in self.soup.select('#meta p') if 'Born:' in x.text][0].replace('Born: ', '').split(' in ')[0].replace('Born:\n', '').replace('\n', '').replace('   ', '')
+            birthday_raw = [x.text for x in self.soup.select('#meta p') if 'Born:' in x.text][0]
         except:
             self.Birthday = ''
+        else:
+            birthday_raw = birthday_raw.replace('Born: ', '').split(' in ')[0]
+            self.Birthday = birthday_raw.replace('Born:\n', '').replace('\n', '').replace('   ', '')
         return self.Birthday
 
+    @debug.error_wrap('executive_info', 'teams', str)
     def teams(self):
         teams = [x.text for x in self.soup.select('tbody th+ .left') if x.text != 'TOT']
         self.Teams = ','.join(list(set(teams)))
@@ -61,6 +76,7 @@ class executive_info:
         self.Executive_ID = prev + 1
         return self.Executive_ID
 
+    @debug.error_wrap('executive_info', 'executive_id', str)
     def executive_id(self):
         return self.Executive_ID
 
@@ -77,10 +93,12 @@ class coach_info:
     def __init__(self, soup):
         self.soup = soup
 
+    @debug.error_wrap('coach_info', 'name', str)
     def name(self):
         self.Name = self.soup.select('h1 span')[0].text
         return self.Name
 
+    @debug.error_wrap('coach_info', 'birthday', str)
     def birthday(self):
         try:
             self.Birthday = [x.text for x in self.soup.select('#meta p') if 'Born:' in x.text][0].replace('Born: ', '').split(' in ')[0].replace('Born:\n', '').replace('\n', '').replace('   ', '')
@@ -88,14 +106,17 @@ class coach_info:
             self.Birthday = ''
         return self.Birthday
 
+    @debug.error_wrap('coach_info', 'wins', int)
     def wins(self):
         self.Wins = int(self.soup.select('.thead .right:nth-child(6)')[0].text)
         return self.Wins
 
+    @debug.error_wrap('coach_info', 'losses', int)
     def losses(self):
         self.Losses = int(self.soup.select('.thead .right:nth-child(7)')[0].text)
         return self.Losses
 
+    @debug.error_wrap('coach_info', 'teams', str)
     def teams(self):
         teams = [x.text for x in self.soup.select('.right+ .left a') if x.text != 'TOT']
         self.Teams = ','.join(list(set(teams)))
@@ -105,6 +126,7 @@ class coach_info:
         self.Coach_ID = prev + 1
         return self.Coach_ID
 
+    @debug.error_wrap('coach_info', 'coach_id', int)
     def coach_id(self):
         return self.Coach_ID
 
