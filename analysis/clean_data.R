81221f9bdd8043de15bf1863eb7a290a827b4fe5
Carson Buttars <carsonbuttars13@gmail.com>
2024-08-12 16:05:12 -0600

    Improve data manipulations and clean up.

--- analysis/clean_data.R
+++ analysis/clean_data.R
@@ -1,6 +1,6 @@
 library(DBI)
 
-conn <- dbConnect(RSQLite::SQLite(), 'bball_db') 
+conn <- dbConnect(RSQLite::SQLite(), '../data/bball_db') 
 game_info <- dbReadTable(conn, "game_info")
 game_info[game_info$Duration == 12360, 'Duration'] <- 126
 dbWriteTable(conn, 'game_info', game_info, overwrite = T)
