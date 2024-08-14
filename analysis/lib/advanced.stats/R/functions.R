c4a8cf8c0f5b283bc4cc6f4ccdbbfc0350fa7d35
Carson Buttars <carsonbuttars13@gmail.com>
2024-08-14 16:08:31 -0600

    Improve offensive rating function and implementation

--- analysis/lib/advanced.stats/R/functions.R
+++ analysis/lib/advanced.stats/R/functions.R
@@ -27,8 +27,8 @@
 # - opponent
 # - league
 
-usage <- function(MP, FGA, FTA, TO, team_MP, team_FGA, team_FTA, team_TO) {
-	numerator <- (FGA + 0.44 * FTA + TO) * team_MP
+usage <- function(MP, FGA, FTA, TOV, team_MP, team_FGA, team_FTA, team_TO) {
+	numerator <- (FGA + 0.44 * FTA + TOV) * team_MP
 	denominator <- (team_FGA + 0.44 * team_FTA + team_TO) * 5 * MP
 	output <- numerator / denominator
 	return(output)
@@ -89,16 +89,45 @@ offensive_rating <- function(Pts, MP, FGM, FGA, Threes, ThreePA, FTM,
 			     FTA, AST, O_REB, TOV, team_Pts, team_MP,
 			     team_FGM, team_FGA, team_Threes, team_ThreePA,
 			     team_FTM, team_FTA, team_AST, team_O_REB,
-			     team_TOV, opp_REB, opp_O_REB) {
+			     team_TOV, opp_D_REB) {
+# prop_FGs "0.409722222222221"
+# prop_ASTs "2.5625"
+# AST_FG_ratio "6.2542372881356"
+# qAST "5.44075211864408"
+# not_FT "0.409090909090909"
+# FGM_part "-0.564405575885989"
+# AST_part "0"
+# FT_pcent "0"
+# FT_part "0"
+# team_FT_pcent "0.736842105263158"
+# team_scoring_poss "45.0736842105263"
+# team_play_pcent "0.34779077322937"
+# team_O_REB_pcent "0.217391304347826"
+# team_O_REB_weight "0.657499189599577"
+# O_REB_part "0"
+# ScPoss "-0.535771609161412"
+# FGMxPoss "4.60434782608696"
+# FTxPoss "0"
+# TotPoss "5.06857621692555"
+# PProd_FGM_part "-1.24169226694918"
+# PProd_AST_part "0"
+# PProd_O_REB_part "0"
+# PProd "-1.17869754015511"
+
+	prop_FGs <- (team_FGM / team_MP) * MP * 5 - FGM
+	prop_ASTs <- (team_AST / team_MP) * MP * 5 - AST
+	AST_FG_ratio <- ifelse(prop_FGs != 0, prop_ASTs / prop_FGs, 0)
 	qAST <- ((MP / (team_MP / 5)) * (1.14 * ((team_AST - AST) / team_FGM))) +
-		((((team_AST / team_MP) * MP * 5 - AST) / ((team_FGM / team_MP) * MP * 5 - FGM)) *
-			(1 - (MP / (team_MP / 5))))
-	FGM_part <- FGM * (1 - 0.5 * ((Pts - FTM) / (2 * FGA)) * qAST)
+		(AST_FG_ratio * (1 - (MP / (team_MP / 5))))
+	not_FT <- ifelse(FGA != 0, (Pts - FTM) / (2 * FGA), 0)
+	FGM_part <- FGM * (1 - 0.5 * (not_FT) * qAST)
 	AST_part <- 0.5 * (((team_Pts - team_FTM) - (Pts - FTM)) / (2 * (team_FGA - FGA))) * AST
-	FT_part <- (1 - (1 - (FTM / FTA))^2) * 0.4 * FTA
-	team_scoring_poss <- team_FGM + (1 - (1 - (team_FTM / team_FTA))^2) * team_FTA * 0.4
+	FT_pcent <- ifelse(FTA != 0, FTM / FTA, 0)
+	FT_part <- (1 - (1 - (FT_pcent))^2) * 0.4 * FTA
+	team_FT_pcent <- ifelse(team_FTA != 0, team_FTM / team_FTA, 0)
+	team_scoring_poss <- team_FGM + (1 - (1 - (team_FT_pcent))^2) * team_FTA * 0.4
 	team_play_pcent <- team_scoring_poss / (team_FGA + team_FTA * 0.4 + team_TOV)
-	team_O_REB_pcent <- team_O_REB / (team_O_REB + (opp_REB - opp_O_REB))
+	team_O_REB_pcent <- team_O_REB / (team_O_REB + opp_D_REB)
 	team_O_REB_weight <- ((1 - team_O_REB_pcent) * team_play_pcent) /
 		((1 - team_O_REB_pcent) * team_play_pcent + team_O_REB_pcent * (1 - team_play_pcent))
 	O_REB_part <- O_REB * team_O_REB_weight * team_play_pcent
@@ -106,19 +135,23 @@ offensive_rating <- function(Pts, MP, FGM, FGA, Threes, ThreePA, FTM,
 		(1 - (team_O_REB / team_scoring_poss) * team_O_REB_weight * team_play_pcent) + O_REB_part
 
 	FGMxPoss <- (FGA - FGM) * (1 - 1.07 * team_O_REB_pcent)
-	FTxPoss <- ((1 - (FTM / FTA))^2) * 0.4 * FTA
+
+	FTxPoss <- ((1 - (FT_pcent))^2) * 0.4 * FTA
 
 	TotPoss <- ScPoss + FGMxPoss + FTxPoss + TOV
 
-	PProd_FGM_part <- 2 * (FGM + 0.5 * Threes) * (1 - 0.5 * ((Pts - FTM) / (2 * FGA)) * qAST)
+	PProd_FGM_part <- 2 * (FGM + 0.5 * Threes) * (1 - 0.5 * not_FT * qAST)
 	PProd_AST_part <- 2 * ((team_FGM - FGM + 0.5 * (team_Threes - Threes)) / (team_FGM - FGM)) *
 		0.5 * (((team_Pts - team_FTM) - (Pts - FTM)) / (2 * (team_FGA - FGA))) * AST
 	PProd_O_REB_part <- O_REB * team_O_REB_weight * team_play_pcent *
-		(team_Pts / (team_FGM + (1 - (1 - (team_FTM / team_FTA))^2) * 0.4 * team_FTA))
+		(team_Pts / (team_FGM + (1 - (1 - (team_FT_pcent))^2) * 0.4 * team_FTA))
+
 	PProd <- (PProd_FGM_part + PProd_AST_part + FTM) *
 		(1 - (team_O_REB / team_scoring_poss) * team_O_REB_weight * team_play_pcent) + PProd_O_REB_part
 
-	ORtg <- 100 * (PProd / TotPoss)
+	pcent <- ifelse(TotPoss != 0, PProd / TotPoss, 0)
+	ORtg <- 100 * (pcent)
+
 	return(ORtg)
 }
 
@@ -131,14 +164,16 @@ defensive_rating <- function(MP, D_REB, STL, BLK, PF, team_MP,
 	DFGM_pcent <- opp_FGM / opp_FGA
 	FMwt <- (DFGM_pcent * (1 - DOR_pcent)) / (DFGM_pcent * (1 - DOR_pcent) + (1 - DFGM_pcent) * DOR_pcent)
 	Stops1 <- STL + BLK * FMwt * (1 - 1.07 * DOR_pcent) + D_REB * (1 - FMwt)
+	PFs <- ifelse(team_PF > 0, PF / team_PF, 0)
+	opp_FT_pcent <- ifelse(opp_FTA > 0, opp_FTM / opp_FTA, 0)
 	Stops2 <- (((opp_FGA - opp_FGM - team_BLK) / team_MP) * FMwt * (1 - 1.07 * DOR_pcent) + ((opp_TOV - team_STL) / team_MP)) *
-		MP + (PF / team_PF) * 0.4 * opp_FTA * (1 - (opp_FTM / opp_FTA))^2
+		MP + (PFs) * 0.4 * opp_FTA * (1 - (opp_FT_pcent))^2
 	Stops <- Stops1 + Stops2
 
-	Stop_pcent <- (Stops * opp_MP) / (team_poss * MP)
+	Stop_pcent <- ifelse(MP > 0, (Stops * opp_MP) / (team_poss * MP), 0)
 
 	team_Defensive_Rating <- 100 * (opp_Pts / team_poss)
-	D_Pts_per_ScPoss <- opp_Pts / (opp_FGM + (1 - (1 - (opp_FTM / opp_FTA))^2) * opp_FTA * 0.4)
+	D_Pts_per_ScPoss <- opp_Pts / (opp_FGM + (1 - (1 - (opp_FT_pcent))^2) * opp_FTA * 0.4)
 
 	DRtg <- team_Defensive_Rating + 0.2 * (100 * D_Pts_per_ScPoss * (1 - Stop_pcent) - team_Defensive_Rating)
 	return(DRtg)
