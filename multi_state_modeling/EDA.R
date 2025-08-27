85cdf9fab33ac5dd81bfae2e9002822febbab7f2
Carson Buttars <carsonbuttars13@gmail.com>
2025-08-27 10:31:31 -0600

    Add lag variables and cut to 14 team fantasy league size.

--- multi_state_modeling/EDA.R
+++ multi_state_modeling/EDA.R
@@ -90,7 +90,9 @@ season_totals <- player_games %>%
       Steals,
       Blocks,
       Turnovers
-    )
+    ),
+    Seconds_lag_two = lag(Seconds, default = 0, order_by = Season),
+    Seconds_lag_three = lag(Seconds, n = 2, default = 0, order_by = Season)
   ) %>%
     select(!c(
       Games,
@@ -107,6 +109,8 @@ data <- transitions %>%
     by = c("Team_ID", "Season"),
     suffix = c("", "_team")
   ) %>%
+  group_by(Season) %>%
+  top_n(182, Fantasy_points) %>% # 182 corresponds to a 14 team league
   select(!c(Player_ID, Team_ID)) %>%
   mutate(
     Win_pct = Wins / (Wins + Losses),
@@ -243,28 +247,31 @@ for (col in train[response_indices] %>% colnames()) {
   # Iterate to next column integer
   i <- i + 1
 }
+# # A tibble: 15 × 4
 #    variable       random_forest full_linear selected_linear
 #    <chr>                  <dbl>       <dbl>           <dbl>
-#  1 mu_hs               7.88e+ 9    1.59e+10        1.60e+10
+#  1 mu_hs               1.11e+10    2.24e+10        2.26e+10
 #  2 mu_hl             Inf         Inf             Inf
-#  3 mu_hS               3.51e+52    6.48e+52        7.15e+52
+#  3 mu_hS               6.99e+52    8.51e+52        9.75e+52
 #  4 mu_sh             Inf         Inf             Inf
-#  5 mu_ls               9.66e+24    1.66e+25        1.69e+25
-#  6 mu_hs_estimate      1.50e- 2    3.04e- 2        3.05e- 2
-#  7 mu_hl_estimate      6.12e- 3    1.34e- 2        1.35e- 2
-#  8 mu_hS_estimate      4.10e- 2    8.29e- 2        8.40e- 2
-#  9 mu_sh_estimate      8.54e- 2    1.98e- 1        1.98e- 1
-# 10 mu_ls_estimate      9.99e- 2    2.23e- 1        2.24e- 1
-# 11 mu_hs_approx        2.34e- 2    5.06e- 2        5.07e- 2
-# 12 mu_hl_approx        1.20e- 2    2.51e- 2        2.52e- 2
-# 13 mu_hS_approx        1.39e- 2    2.75e- 2        2.76e- 2
-# 14 mu_sh_approx        1.45e- 1    3.46e- 1        3.48e- 1
-# 15 mu_ls_approx        1.08e- 1    2.51e- 1        2.54e- 1
+#  5 mu_ls               3.32e+18    7.99e+18        8.34e+18
+#  6 mu_hs_estimate      5.04e- 3    1.15e- 2        1.16e- 2
+#  7 mu_hl_estimate      3.59e- 3    7.59e- 3        7.73e- 3
+#  8 mu_hS_estimate      2.52e- 2    4.45e- 2        4.69e- 2
+#  9 mu_sh_estimate      8.53e- 2    1.97e- 1        1.98e- 1
+# 10 mu_ls_estimate      1.06e- 1    2.34e- 1        2.36e- 1
+# 11 mu_hs_approx        1.17e- 2    2.67e- 2        2.68e- 2
+# 12 mu_hl_approx        7.22e- 3    1.47e- 2        1.47e- 2
+# 13 mu_hS_approx        1.13e- 2    2.25e- 2        2.27e- 2
+# 14 mu_sh_approx        1.40e- 1    3.36e- 1        3.39e- 1
+# 15 mu_ls_approx        1.02e- 1    2.39e- 1        2.47e- 1
 
 # These models seem to have a very hard time explaining much variation at all.
 # The linear models don't perform much worse than the random forest, and they
 # all had an R^2 less than 0.07, which needs to be more to be useful. However,
-# I'll look for what variables these models think are most important.
+# it may be interesting to see how it does with a transformation of the response
+# variable or using a logistic regression. For the time being, I'll just take
+# note of what variables were commonly selected in the various models.
 for (item in models) {
   name <- item$name
   vars <- item$selected_vars
@@ -272,19 +279,18 @@ for (item in models) {
   paste(name, ":", vars, "\n") %>%
     cat()
 }
-
 # mu_hs : Assists
 # mu_hl : *Ignored*
-# mu_hS : 1
+# mu_hS : Draft_Year
 # mu_sh : *Ignored*
 # mu_ls : 1
-# mu_hs_estimate : Season + Seconds + Games_played
-# mu_hl_estimate : Fouls + Games_played
-# mu_hS_estimate : Games_played
-# mu_sh_estimate : Games_played + Age
-# mu_ls_estimate : Assists + Turnovers + Games_played
-# mu_hs_approx : Season + Seconds + Points + Games_played + Season_start
-# mu_hl_approx : Season + Fouls + Games_played + Season_start + Age
+# mu_hs_estimate : Season + Points + Games_played + Seconds_lag_two + Age
+# mu_hl_estimate : Draft_Year
+# mu_hS_estimate : 1
+# mu_sh_estimate : Seconds_lag_two + Age + Seconds
+# mu_ls_estimate : Assists + Turnovers
+# mu_hs_approx : Season + Points + Games_played + Season_start + Seconds_lag_two + Age
+# mu_hl_approx : Season + Games_played
 # mu_hS_approx : Games_played
 # mu_sh_approx : Season + Experience
 # mu_ls_approx : 1
